bd7061623db7a3d81a7345b561b16455db3b0eba
Veeradharshini <dharshinirajenderan@gmail.com>
2024-11-21 21:37:33 +0530

    Built in Types(string)

---
+++ Built in types/Built in types/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class StringProgram
+{
+    static void Main()
+    {
+        string str = "Veera dharshini";
+        Console.WriteLine("Original String: " + str);
+        Console.WriteLine("Upper Case: " + str.ToUpper());
+        Console.WriteLine("Lower Case: " + str.ToLower());
+        Console.WriteLine("Length: " + str.Length);
+        Console.WriteLine("Contains 'Dharshini': " + str.Contains("Dharshini"));
+        Console.WriteLine("IndexOf 'Veera': " + str.IndexOf("veera"));
+        Console.WriteLine("Substring 6: " + str.Substring(6));
+        Console.WriteLine("Replace 'dharshini' with 'harshini': " + str.Replace("dharshini", "harshini"));
+        Console.WriteLine("Trim: " + str.Trim());
+        Console.WriteLine("Split: " + str.Split(' ')[0]);
+    }
+}
---
+++ read and write/read and write/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Read and Write");
+        Console.WriteLine("Enter your name:");
+        string name = Console.ReadLine();
+        Console.WriteLine("Hello, " + name + "!");
+    }
+}
