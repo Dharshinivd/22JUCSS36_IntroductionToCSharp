5886b7c075dbbfc6a038a30ee9be594caf15e597
Veeradharshini <dharshinirajenderan@gmail.com>
2024-11-21 22:16:38 +0530

    Single dimensional array

---
+++ arrays/arrays/Program.cs
@@ -0,0 +1,7 @@
+﻿
+int[] numbers = new int[5]; 
+numbers[0] = 10;
+numbers[1] = 20;
+int[] directNumbers = { 10, 20, 30, 40, 50 };
+Console.WriteLine($"First Element: {numbers[0]}");
+Console.WriteLine($"Second Element: {numbers[1]}");
