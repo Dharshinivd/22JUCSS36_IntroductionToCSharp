545a46ae2b083ae35fa8acda279f4ed486db72a9
Veeradharshini <dharshinirajenderan@gmail.com>
2024-11-21 22:21:01 +0530

    Multi dimensional array

---
+++ array/array/Program.cs
@@ -0,0 +1,8 @@
+﻿
+int[,] matrix = new int[2, 3]
+{
+    { 1, 2, 3 },   
+    { 4, 5, 6 }     
+};
+Console.WriteLine($"Element at [0,0]: {matrix[0, 0]}");
+Console.WriteLine($"Element at [1,2]: {matrix[1, 2]}");
