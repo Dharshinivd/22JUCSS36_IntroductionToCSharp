8b718cec9420e6d7d95d7876860326dd26b1c5c0
Veeradharshini <dharshinirajenderan@gmail.com>
2024-11-21 22:08:51 +0530

    Data Type

---
+++ data type/data type/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class data
+{
+    static void Main()
+    {
+        int intVar = 42;
+        double doubleVar = intVar; 
+        Console.WriteLine($"Implicit Conversion: int {intVar} to double {doubleVar}");
+
+        double largeDouble = 123.45;
+        int smallInt = (int)largeDouble;
+        Console.WriteLine($"Explicit Conversion: double {largeDouble} to int {smallInt}");
+
+        string str = "100";
+        int strToInt = Convert.ToInt32(str);
+        Console.WriteLine($"Convert String to Int: {strToInt}");  
+    }
+}
