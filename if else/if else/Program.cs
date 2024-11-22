2930ff9c3b12ea2ee014f31b873c95db2c6f8893
Veeradharshini <dharshinirajenderan@gmail.com>
2024-11-22 10:23:07 +0530

    if...else

---
+++ if else/if else/Program.cs
@@ -0,0 +1,11 @@
+﻿
+       int x = 20;
+       int y = 18;
+       if(x>y)
+          {
+            Console.WriteLine("x is greater than y");
+           }
+       else
+           {
+            Console.WriteLine("x is smaller than y");
+           }
