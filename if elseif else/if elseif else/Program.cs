1d5d7eaae914bc237542bb8c0056357ce815ec54
Veeradharshini <dharshinirajenderan@gmail.com>
2024-11-22 10:47:19 +0530

    if...elseif...else

---
+++ if elseif else/if elseif else/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter your age:");
+        int age=int.Parse(Console.ReadLine());
+        if (age < 13)
+        {
+            Console.WriteLine("You are a child.");
+        }
+        else if(age >= 13 && age < 18)
+        {
+            Console.WriteLine("You are a teenager.");
+        }
+        else
+        {
+            Console.WriteLine("You are an adult.");
+        }
+
+
+    }
+}
+    
