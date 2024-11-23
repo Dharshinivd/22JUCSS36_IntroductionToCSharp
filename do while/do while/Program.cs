8c76186d2db0c5ffe031c6c56738594bc864fb5b
Veeradharshini <dharshinirajenderan@gmail.com>
2024-11-23 21:30:49 +0530

    Do While

---
+++ do while/do while/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+  class Dowhile
+{
+    static void Main(string[] args)
+    {
+        int i = 0;
+        do
+        {
+            Console.WriteLine(i);
+            i++;
+        }
+        while (i < 5);
+    }
+}
