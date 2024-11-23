5f60c1db7a5449eea238ba5b91e0d19a2e916d8d
Veeradharshini <dharshinirajenderan@gmail.com>
2024-11-23 21:58:59 +0530

    Method Parameters

---
+++ MethodParameters/MethodParameters/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class Parameter
+{
+    static void MyMethod(string name)
+    {
+        Console.WriteLine(name+ "Dharshini");
+    }
+    static void Main(string[] args)
+    {
+        MyMethod("Veera");
+        MyMethod("Priya");
+        MyMethod("Divya");
+    }
+}
