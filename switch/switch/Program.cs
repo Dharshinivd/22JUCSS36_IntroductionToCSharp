1dd2129f52c9acc8011616d812ce6f5134881366
Veeradharshini <dharshinirajenderan@gmail.com>
2024-11-23 21:10:06 +0530

    Switch statement

---
+++ switch/switch/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Switch
+{
+    static void Main(string[] args)
+    {
+        int day = 4;
+        switch (day)
+        {
+            case 1:
+                Console.WriteLine("It's Sunday!");
+                break;
+            case 2:
+                Console.WriteLine("It's Monday!");
+                break;
+            case 3:
+                Console.WriteLine("It's Tuesday");
+                break;
+            case 4:
+                Console.WriteLine("It's Wednesday");
+                break;
+            case 5:
+                Console.WriteLine("It's Thursday");
+                break;
+        }
+    }
+}
