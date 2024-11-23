0e67d4d28a168072b86861db7dd3b94844a7e37b
Veeradharshini <dharshinirajenderan@gmail.com>
2024-11-23 21:10:39 +0530

    Comments in c#

--- comments/comments/Program.cs
+++ comments/comments/Program.cs
@@ -6,8 +6,8 @@
         Console.WriteLine("Hello World!");//Prints a message to the console
         Console.WriteLine("Hello World!");/* This is a multi-line comment
                                             * It can span several lines*/
-
-
+    
+    
     }
 }
 
