26b8a242c1684c4d22b3ecbf45309857d9008271
Veeradharshini <dharshinirajenderan@gmail.com>
2024-11-23 21:39:59 +0530

    foreach loop

---
+++ for each/for each/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class Foreach
+{
+    static void Main(string[] args)
+    {
+        string[] icecream = { "Vannilla", "Black Current", "Strwberry", "Chocolate" };
+        foreach (string ice in icecream)
+        {
+            Console.WriteLine(ice);
+        }
+    }
+}
