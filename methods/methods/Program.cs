e7dcc79ee078ebeaba418d8bd0a409e69804e3a9
Veeradharshini <dharshinirajenderan@gmail.com>
2024-11-23 21:51:31 +0530

    Methods in c#

---
+++ methods/methods/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class Method
+{
+    static void MyMethod()
+    {
+        Console.WriteLine("It's Executed.");
+    }
+    static void MyName()
+    {
+        Console.WriteLine("Another Example.");
+        int i = 0;
+        while (i<5)
+        {
+           Console.WriteLine( i);i++;
+        }
+    }
+    static void Main(string[] args)
+    {
+        MyMethod();
+        MyName();
+    }
+}
