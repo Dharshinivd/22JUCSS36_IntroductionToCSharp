7cf7becdcace8be5fb1fb9ad3b548795508affa5
Veeradharshini <dharshinirajenderan@gmail.com>
2024-11-23 20:53:49 +0530

    Comments in c#

---
+++ comments/comments/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+    class Comment
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello World!");//Prints a message to the console
+        Console.WriteLine("Hello World!");/* This is a multi-line comment
+                                            * It can span several lines*/
+
+
+    }
+}
+
+
