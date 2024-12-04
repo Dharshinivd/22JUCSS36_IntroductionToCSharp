1c5dd1f1b64a6d940b274bfd44290675da7e7f2f
Veeradharshini <dharshinirajenderan@gmail.com>
2024-12-04 14:49:59 +0530

    mvcDemo

--- mvcDemo/mvcDemo/Controllers/Staffs.cs
+++ mvcDemo/mvcDemo/Controllers/Staffs.cs
@@ -1,24 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using mvcDemo.Models;
 using mvcDemo.Data;
-using mvcDemo.Models;
 using mvcDemo.Models;
 using mvcDemo.Data;
 using mvcDemo.Models;
 
 namespace MVCDemo.Controllers
 {
-    public class StaffsController : Controller
+
+
+    public class Staffs : Controller
     {
         private readonly ApplicationDBContext _context;
-
-        public StaffsController(ApplicationDBContext context)
+        public Staffs(ApplicationDBContext context)
         {
             _context = context;
         }
@@ -26,10 +20,7 @@ namespace MVCDemo.Controllers
         public async Task<IActionResult> Index()
         {
             return View(await _context.Staffs.ToListAsync());
-        }
-
-        // GET: Students/Create
-        public IActionResult Create()
+        }public IActionResult Create()
         {
             return View();
         }
@@ -45,6 +36,89 @@ namespace MVCDemo.Controllers
                 return RedirectToAction(nameof(Index));
             }
             return View(staff);
+        }
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var staff = await _context.Staffs.FindAsync(id);
+            if (staff == null)
+            {
+                return NotFound();
+            }
+            return View(staff);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,DepartmentId,StaffName,Qualification")] Staff staff)
+        {
+            if (id != staff.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(staff);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!StaffExists(staff.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(staff);
+
+        }
+        // GET: Students/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var staff = await _context.Staffs
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (staff == null)
+            {
+                return NotFound();
+            }
+
+            return View(staff);
+        }
+        // POST: Students/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var staffs = await _context.Staffs.FindAsync(id);
+            if (staffs != null)
+            {
+                _context.Staffs.Remove(staffs);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        private bool StaffExists(int id)
+        {
+            return _context.Staffs.Any(e => e.Id == id);
         }
 
     }
