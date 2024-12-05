6323653c1f4fb4b8a805c5eefc7b778945955c3a
Veeradharshini <dharshinirajenderan@gmail.com>
2024-12-05 13:52:10 +0530

    Polymorphism in c#

---
+++ polymorphism/polymorphism/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+public class Animal
+{
+    public virtual void eat()
+    {
+        Console.WriteLine("eating...");
+    }
+}
+public class Dog : Animal
+{
+    public override void eat()
+    {
+        Console.WriteLine("EGG");
+    }
+
+
+}
+public class Elephant : Animal
+{
+    public override void eat()
+    {
+        Console.WriteLine("Sugarcane");
+    }
+
+
+}
+public class TestPolymorphism
+{
+    public static void Main()
+    {
+        Animal a = new Dog();
+        a.eat();
+        Animal b = new Elephant();
+        b.eat();
+    }
+}
